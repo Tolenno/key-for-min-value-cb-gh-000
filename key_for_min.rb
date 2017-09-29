863665577b8fd4f21c4e3b2bef5be8161f565595
Matt Hough <matt@hough.tv>
2017-09-29 12:42:04 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,8 @@ def key_for_min_value(name_hash)
   winner = nil
   name_hash.each do |key, value|
     if value > compare
-      compare = value && winner = key
+      compare = value
+      winner = key
     end
   end
   winner
